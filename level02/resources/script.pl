d4223b754b578bee7deafca778f96c52dc975a8b
Louis <louis@interactivelayer.com>
2024-01-20 15:52:47 +0100

    save

--- level02/resources/script.pl
+++ level02/resources/script.pl
@@ -1,10 +1,8 @@
 #!/usr/bin/perl
 
 $ip = @ARGV ? $ARGV[0] : "192.168.56.2";
-$file = "level02.pcap";
-system("scp -q -P 4242 level02\@$ip:/home/user/level02/$file .");
-system("chmod 777 $file && file $file > ./log"); # basic informations
+system("ssh -qp 4242 level02\@$ip 'tcpdump -r level02.pcap -A'");
+#system("file $file > ./log"); # basic informations")
 #system("ldd $file >> ./log"); # dynamic libraries
 #system("objdump -d $file >> ./log"); # assembly code
 #system("strings $file >> ./log"); # raw strings
-system("cat log && rm -rf level02.pcap log");
