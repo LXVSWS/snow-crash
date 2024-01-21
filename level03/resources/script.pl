8965d24d1f25218d9068bda54d233557af7064e2
Louis <louis@interactivelayer.com>
2024-01-21 23:50:30 +0100

    save

--- level03/resources/script.pl
+++ level03/resources/script.pl
@@ -1,4 +1,8 @@
-#system("file $file > ./log"); # basic informations")
-#system("ldd $file >> ./log"); # dynamic libraries
+#!/usr/bin/perl
+
+$ip = @ARGV ? $ARGV[0] : "192.168.56.2";
+$file = "level03";
+system("ssh -qp 4242 level03\@$ip 'file $file'"); # basic informations
+# system("ldd $file"); # dynamic libraries
 #system("objdump -d $file >> ./log"); # assembly code
 #system("strings $file >> ./log"); # raw strings
