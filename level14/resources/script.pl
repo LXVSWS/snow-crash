e6d5d786985d41d85349d8bda24a7d77a7ed2365
LXV <lxvsws@gmail.com>
2024-02-19 14:10:35 +0100

    save

---
+++ level14/resources/script.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+
+$ip = @ARGV ? $ARGV[0] : "192.168.56.2";
+$user = "level14";
+
+$red = "\e[31m";
+$green = "\e[32m";
+$reset = "\e[0m";
+
+$scp = "scp -q -P 4242 $user\@$ip:/bin/getflag .";
+print $red, $scp, $reset, "\n";
+system($scp);
