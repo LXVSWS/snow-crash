a92bf8551574e932476ab84b6e5b169a8f870f7c
Louis Wyss <lwyss@c4r1p1.42nice.fr>
2024-04-22 22:02:43 +0200

    save

--- level02/resources/script.pl
+++ level02/resources/script.pl
@@ -7,8 +7,5 @@ $green = "\e[32m";
 $reset = "\e[0m";
 $file = "level02.pcap";
 
-$cmd = "scp -q -P $port level02\@$ip:~/$file .";
-system($cmd);
-print $red, $cmd, "\n", $reset;
 print $green, "ft_waNDReL0L\n", $reset;
 system("ssh -qp $port flag02\@$ip 'getflag'");
